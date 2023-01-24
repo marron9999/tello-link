8ea33dbbcb161beaddd804207a09fc36b55afa10
marron9999 <hiroyuki.inaba@gmail.com>
2023-01-25 07:49:37 +0900

    Update copy_to_test.bat

--- copy_to_test.bat
+++ copy_to_test.bat
@@ -1,7 +1,8 @@
-mkdir C:\java\github\tello-test
-mkdir C:\java\github\tello-test\tello
-cd /d C:\java\github\tello-test
-xcopy C:\java\github\tello-link\tello-emu\bin\Release\net7.0-windows\*.* . /s /y
-xcopy C:\java\github\tello-link\tello-link\bin\Release\net7.0-windows\*.* . /s /y
-xcopy C:\java\github\tello-link\tello-html\*.* tello\ /s /y
+cd /d %~dp0
+mkdir bin
+mkdir bin\tello
+cd bin
+xcopy ..\tello-emu\bin\Release\net7.0-windows\*.* . /s /y
+xcopy ..\tello-link\bin\Release\net7.0-windows\*.* . /s /y
+xcopy ..\tello-link\tello-html\*.* tello\ /s /y
 pause
